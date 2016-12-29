8c150e847999484eebd5cb84254f7776277f0f76
Gregory Shimansky <gregory.shimansky@intel.com>
2016-12-29 13:32:20 -0600

    Done division with remainder for single digits

--- solve.go
+++ solve.go
@@ -41,31 +41,30 @@ th, td {
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	count := 0
-	answers := make([][3]int, NUM_IN_ROW * numlines)
+	answers := make([][4]int, NUM_IN_ROW * numlines)
 	for i := int64(0); i < numlines; i++ {
 		fmt.Fprint(out, "<tr>")
 		for j := 0; j < NUM_IN_ROW; j++ {
 			first := r.Intn(9) + 1
 			second := r.Intn(9) + 1
-			mult := first * second
+			rem := r.Intn(second)
+			mult := first * second + rem
+			pad := ""
 
 			if mult < 10 {
-				fmt.Fprintf(out, `<td><code>%d)</br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</u></br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%d)&nbsp;&nbsp;&nbsp;%d</br>
-</br>
-</br></code></td>`, count + 1, first, mult)
-			} else {
-				fmt.Fprintf(out, `<td><code>%d)</br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</u></br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%d)&nbsp;&nbsp;%d</br>
-</br>
-</br></code></td>`, count + 1, first, mult)
+				pad = "&nbsp;"
 			}
+
+			fmt.Fprintf(out, `<td><code>%d)</br>
+&nbsp;&nbsp;&nbsp;&nbsp;<u>&nbsp;&nbsp;&nbsp;&nbsp;</u>&nbsp;r&nbsp;&nbsp;</br>
+&nbsp;&nbsp;&nbsp;%d)&nbsp;%s%d</br>
+</br>
+</br></code></td>`, count + 1, first, pad, mult)
 
 			answers[count][0] = mult
 			answers[count][1] = first
 			answers[count][2] = second
+			answers[count][3] = rem
 			count++
 		}
 		fmt.Fprint(out, "</tr>")
@@ -85,7 +84,7 @@ th, td {
 		if answers[i][0] < 10 {
 			pad_first = "&nbsp;"
 		}
-		fmt.Fprintf(out, "%s%d) %s%d &divide; %d = %d</br>\n", pad_count, i + 1, pad_first, answers[i][0], answers[i][1], answers[i][2])
+		fmt.Fprintf(out, "%s%d) %s%d &divide; %d = %d r %d</br>\n", pad_count, i + 1, pad_first, answers[i][0], answers[i][1], answers[i][2], answers[i][3])
 	}
 	fmt.Fprint(out, "</tt></body></html>")
 	out.Close()
